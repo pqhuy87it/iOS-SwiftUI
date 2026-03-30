4cd82ba308b4df993e6d73231efda291a58406de
Phạm Huy(みかん坊や) <pqhuy87it@gmail.com>
2026-03-30 14:54:56 +0700

    create project

---
+++ SwiftUISampleCode/SwiftUISampleCode/SwiftUISampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleCodeApp.swift
+//  SwiftUISampleCode
+//
+//  Created by huy on 2026/03/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
